5507ef8c7bff5dce38e3b027ed684738bbeed8f6
keiya01 <keiya.s.0210@gmail.com>
2020-01-24 12:50:33 +0900

    Update to refactor

--- main.go
+++ main.go
@@ -1,39 +1,52 @@
 package main
 
 import (
-	"log"
-	"net/http"
 	"fmt"
 	"github.com/gorilla/mux"
+	"log"
+	"net/http"
 )
 
-type server struct{}
+func get(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`{"message": "Received GET method"}`))
+}
 
-func home(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
+func post(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`{"message": "Received POST method"}`))
+}
 
-	switch r.Method {
-	case "GET":
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(`{"message": "Received GET method"}`))
-	case "POST":
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(`{"message": "Received POST method"}`))
-	case "PUT":
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(`{"message": "Received PUT method"}`))
-	case "DELETE":
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(`{"message": "Received DELETE method"}`))
-	default:
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte(`{"message": "Received method not found"}`))
-	}
+func put(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`{"message": "Received PUT method"}`))
+}
+
+func delete(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`{"message": "Received DELETE method"}`))
+}
+
+func notFound(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(`{"message": "Received method not found"}`))
+}
+
+func json(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		next.ServeHTTP(w, r)
+	})
 }
 
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/", home)
+	r.Use(json)
+	api := r.PathPrefix("/api/v1").Subrouter()
+	api.HandleFunc("/", get).Methods(http.MethodGet)
+	api.HandleFunc("/", post).Methods(http.MethodPost)
+	api.HandleFunc("/", put).Methods(http.MethodPut)
+	api.HandleFunc("/", delete).Methods(http.MethodDelete)
+	api.HandleFunc("/", notFound)
 	fmt.Println("Listening on 8080")
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
