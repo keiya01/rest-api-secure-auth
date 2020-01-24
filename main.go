1d572ad644fd93046eb7f5b72f18d6484d77f7e3
keiya01 <keiya.s.0210@gmail.com>
2020-01-24 11:53:30 +0900

    Add gorilla/mux package

--- main.go
+++ main.go
@@ -3,22 +3,37 @@ package main
 import (
 	"log"
 	"net/http"
+	"fmt"
+	"github.com/gorilla/mux"
 )
 
 type server struct{}
 
-func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
+func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	w.Write([]byte(`{"message": "Hello World!"}`))
+
+	switch r.Method {
+	case "GET":
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"message": "Received GET method"}`))
+	case "POST":
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"message": "Received POST method"}`))
+	case "PUT":
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"message": "Received PUT method"}`))
+	case "DELETE":
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"message": "Received DELETE method"}`))
+	default:
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(`{"message": "Received method not found"}`))
+	}
 }
 
 func main() {
-	s := &server{}
-	http.Handle("/", s)
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Printf("Listening on 8080")
+	r := mux.NewRouter()
+	r.HandleFunc("/", home)
+	fmt.Println("Listening on 8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
