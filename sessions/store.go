2dfbe649225ca0c9b4387bf95edfd78b74a0b7f2
keiya01 <keiya.s.0210@gmail.com>
2020-01-26 10:23:43 +0900

    Fix session to mutate in global

--- sessions/store.go
+++ sessions/store.go
@@ -1,10 +1,26 @@
 package sessions
 
 import (
+	"net/http"
 	"github.com/gorilla/securecookie"
 	"github.com/gorilla/sessions"
 )
 
+var store = &sessions.CookieStore{}
+
 func NewStore() *sessions.CookieStore {
-	return sessions.NewCookieStore(securecookie.GenerateRandomKey(32))
+	return sessions.NewCookieStore(securecookie.GenerateRandomKey(64), securecookie.GenerateRandomKey(32))
+}
+
+
+func SetSessionStore(sessionStore *sessions.CookieStore) {
+	store = sessionStore
+}
+
+func Get(r *http.Request, name string) (*sessions.Session, error) {
+	return store.Get(r, name)
+}
+
+func Save(r *http.Request, w http.ResponseWriter, session *sessions.Session) error {
+	return store.Save(r, w, session)
 }
