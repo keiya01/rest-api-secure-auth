338917d54ad7ce3ea1389c0275b012587104ccf2
keiya01 <keiya.s.0210@gmail.com>
2020-01-26 15:37:48 +0900

    Fix database package

--- database/database.go
+++ database/database.go
@@ -4,19 +4,25 @@ type MockDB struct {
 	Values map[string]interface{}
 }
 
+var db = &MockDB{}
+
 func NewDB() *MockDB {
 	return &MockDB{
 		Values: map[string]interface{}{},
 	}
 }
 
-func (db *MockDB) Get(val string) interface{} {
+func SetDB(_db *MockDB) {
+	db = _db
+}
+
+func Get(val string) interface{} {
 	if data, ok := db.Values[val]; ok {
 		return data
 	}
 	return nil
 }
 
-func (db *MockDB) Insert(key string, val interface{}) {
+func Insert(key string, val interface{}) {
 	db.Values[key] = val
 }
