b8253a48bc60dba9bf60e709f30fa4ab02114e3e
keiya01 <keiya.s.0210@gmail.com>
2020-01-27 18:17:16 +0900

    Add AuthProvider function to auth/provider

--- auth/provider.go
+++ auth/provider.go
@@ -1,11 +1,23 @@
 package auth
 
 import (
+	"github.com/keiya01/rest-api-secure-auth/model"
 	"github.com/markbates/goth"
+	"github.com/markbates/goth/gothic"
 	"github.com/markbates/goth/providers/twitter"
+	"net/http"
 	"os"
 )
 
 func SetProvider() {
 	goth.UseProviders(twitter.New(os.Getenv("TWITTER_CLIENT_KEY"), os.Getenv("TWITTER_SECRET_KEY"), "http://localhost:8080/api/v1/login/twitter/callback"))
+}
+
+func AuthProvider(w http.ResponseWriter, r *http.Request) (model.User, bool) {
+	gothUser, err := gothic.CompleteUserAuth(w, r)
+	if err != nil {
+		return model.User{}, false
+	}
+
+	return model.NewUser(gothUser.UserID,gothUser.Name,gothUser.Description), true
 }
