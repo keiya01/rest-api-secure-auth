f2b66bf6f365b33d12d9478108e5e9cd68d65d43
keiya01 <keiya.s.0210@gmail.com>
2020-01-28 12:32:01 +0900

    Add UseCSRFToken function

--- response/header.go
+++ response/header.go
@@ -5,6 +5,10 @@ import (
 	"net/http"
 )
 
+func UseCSRFToken(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("X-CSRF-Token", csrf.Token(r))
+}
+
 func UseJSON(w http.ResponseWriter) {
 	w.Header().Set("Content-Type", "application/json")
 }
