bd6b316001a10d8f51191c305c021da404ab7f2b
keiya01 <keiya.s.0210@gmail.com>
2020-01-28 13:30:07 +0900

    Add Email property to User model

--- model/user.go
+++ model/user.go
@@ -4,12 +4,14 @@ type User struct {
 	ID          string `json:"id"`
 	Name        string `json:"name"`
 	Description string `json:"description"`
+	Email       string `json:"email"`
 }
 
-func NewUser(id string, name string, description string) User {
+func NewUser(id string, name string, description string, email string) User {
 	return User{
 		ID:          id,
 		Name:        name,
 		Description: description,
+		Email:       email,
 	}
 }
