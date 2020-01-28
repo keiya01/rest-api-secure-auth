0ac411f73382ae7e907a28eacbf73602b6da4755
keiya01 <keiya.s.0210@gmail.com>
2020-01-28 13:30:27 +0900

    Add AutoLogin function

--- router/auth.go
+++ router/auth.go
@@ -1,12 +1,21 @@
 package router
 
 import (
-	"net/http"
 	"github.com/keiya01/rest-api-secure-auth/handler"
+	"net/http"
 )
 
 func (r *Router) auth() {
+	authRouter := r.PathPrefix("/auth").Subrouter()
 	a := handler.NewAuthHandler()
-	r.HandleFunc("/login/{provider}", a.Login).Methods(http.MethodGet)
-	r.HandleFunc("/login/{provider}/callback", a.AuthCallback).Methods(http.MethodGet)
+	authRouter.Use(func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if !a.HandleAutoLogin(w, r) {
+				next.ServeHTTP(w, r)
+			}
+		})
+	})
+	authRouter.HandleFunc("/{provider}", a.HandleExternalLogin).Methods(http.MethodGet)
+	authRouter.HandleFunc("/", a.HandleLogin).Methods(http.MethodPost)
+	authRouter.HandleFunc("/{provider}/callback", a.AuthCallback).Methods(http.MethodGet)
 }
