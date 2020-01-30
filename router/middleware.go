1a50ec796590f0c34d0e04d174b84efdd32ad913
keiya01 <keiya.s.0210@gmail.com>
2020-01-30 14:23:53 +0900

    Add cors

--- router/middleware.go
+++ router/middleware.go
@@ -6,11 +6,17 @@ import (
 	"net/http"
 )
 
-func useCSRF() func(http.Handler) http.Handler {
-	// TODO: Remove csrf.Secure in production
-	return csrf.Protect(crypto.GenerateRandomKey(32), csrf.Secure(false))
+func cors(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
+		w.Header().Set("Access-Control-Allow-Headers", "Origin, Authorization, Accept, Content-Type")
+		w.Header().Set("Access-Control-Max-Age", "86400") // Time to cache preflight request
+		next.ServeHTTP(w, r)
+	})
 }
 
 func (r *Router) middleware() {
-	r.Use(useCSRF())
+	r.Use(cors)
+	r.Use(csrf.Protect(crypto.GenerateRandomKey(32), csrf.Secure(false))) // TODO: Remove csrf.Secure in production
 }
