baeccdc4f405a465bd2a4d0e4fd024140748f090
keiya01 <keiya.s.0210@gmail.com>
2020-02-01 21:41:09 +0900

    Fix cors

--- router/middleware.go
+++ router/middleware.go
@@ -10,9 +10,15 @@ func cors(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
-		w.Header().Set("Access-Control-Allow-Headers", "Origin, Authorization, Accept, Content-Type")
+		w.Header().Set("Access-Control-Allow-Headers", "Origin, Authorization, Accept, Content-Type, X-CSRF-Token")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+		w.Header().Set("Access-Control-Expose-Headers", "X-CSRF-Token")
 		w.Header().Set("Access-Control-Max-Age", "86400") // Time to cache preflight request
-		next.ServeHTTP(w, r)
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusOK)
+		} else {
+			next.ServeHTTP(w, r)
+		}
 	})
 }
 
@@ -28,7 +34,13 @@ func common(next http.Handler) http.Handler {
 }
 
 func (r *Router) middleware() {
-	r.Use(cors)
-	r.Use(common)
-	r.Use(csrf.Protect(crypto.GenerateRandomKey(32), csrf.Secure(false))) // TODO: Remove csrf.Secure in production
+	r.Use(
+		cors,
+		common,
+		csrf.Protect(
+			crypto.GenerateRandomKey(32),
+			csrf.Secure(false), // TODO: Remove csrf.Secure in production
+			csrf.TrustedOrigins([]string{"localhost:3000"}),
+		),
+	)
 }
